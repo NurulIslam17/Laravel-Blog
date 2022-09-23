3f8b4515897c42c5c6c2f0634fe6235c06738183
Md Nurul Islam <nurulcse09@gmail.com>
2022-09-23 14:28:34 +0600

    Laravel Blog Set Up

---
+++ biztrox/resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,10 @@
+
+@extends('admin.master')
+
+@section('title')
+    Admin Home
+@endsection
+
+@section('content')
+    <h2 class="text-center">Admin Home</h2>
+@endsection
---
+++ biztrox/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AdminController;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+
+    Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
+});
